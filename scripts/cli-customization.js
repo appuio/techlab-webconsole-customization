6a98ef031bb59ff7af4873aca835bb024c5398d8
Benjamin Affolter <bliemli@users.noreply.github.com>
2018-08-21 13:42:59 +0200

    Add scripts

---
+++ scripts/cli-customization.js
@@ -0,0 +1,5 @@
+window.OPENSHIFT_CONSTANTS.CLI = {
+  "Linux":    "https://cdn.techlab-apps.openshift.ch/openshift-client/linux/oc",
+  "Windows":  "https://cdn.techlab-apps.openshift.ch/openshift-client/windows/oc",
+  "Mac OS X": "https://cdn.techlab-apps.openshift.ch/openshift-client/macosx/oc"
+};
---
+++ scripts/menu-customization.js
@@ -0,0 +1,11 @@
+window.OPENSHIFT_CONSTANTS.APP_LAUNCHER_NAVIGATION = [{
+  title: "Techlab",
+  iconClass: "pficon pficon-catalog",
+  href: "https://github.com/appuio/techlab",
+  tooltip: 'Access the labs'
+}, {
+  title: "Survey",
+  iconClass: "fa fa-check",
+  href: "https://survey.puzzle.ch/index.php/124513",
+  tooltip: "Take the survey"
+}];
