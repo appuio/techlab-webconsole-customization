4640384dd0b6e217982152b63afb4e65945e3618
Benjamin Affolter <bliemli@users.noreply.github.com>
2019-02-25 10:08:18 +0100

    Remove survey link

--- content/techlab-extensions.js
+++ content/techlab-extensions.js
@@ -11,9 +11,4 @@ window.OPENSHIFT_CONSTANTS.APP_LAUNCHER_NAVIGATION = [{
   iconClass: "pficon pficon-catalog",
   href: "https://github.com/appuio/techlab",
   tooltip: 'Access the labs'
-}, {
-  title: "Survey",
-  iconClass: "fa fa-check",
-  href: "https://survey.puzzle.ch/index.php/124513",
-  tooltip: "Take the survey"
 }];
